25261394b9d9c818870d83b30442798a58b9d3b5
Dinesh <dineshkumar.software@gmail.com>
2021-12-01 15:33:56 +0530

    New inputs

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public PlayerInput m_PlayerInput;
+    private InputAction m_Steer;
+    private InputAction m_Throttle;
+    private InputAction m_Brake;
+
+    public float Throttle;
+    public float Steer;
+    public float Brake;
+
+    private void Awake()
     {
-        
+        m_Throttle = m_PlayerInput.actions["Throttle"];
+        m_Steer = m_PlayerInput.actions["Steer"];
+        m_Brake = m_PlayerInput.actions["Brake"];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Throttle = m_Throttle.ReadValue<float>();
     }
+
 }
